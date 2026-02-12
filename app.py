17c3b966aa9d7cce7851b118c5ea316b4ef7b43e
Rahul gowda <gowdarahul185@gmail.com>
2026-02-12 19:13:27 +0530

    added

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
+# Home route
+@app.route("/")
 def home():
-    return jsonify({"message": "Flask Backend Running with Docker"})
+    return "Hello from Flask!"
+
+# API route
+@app.route("/api", methods=["GET"])
+def api():
+    return jsonify({
+        "message": "This is a Flask API",
+        "method": "GET"
+    })
+
+# POST example
+@app.route("/data", methods=["POST"])
+def receive_data():
+    data = request.json
+    return jsonify({
+        "received": data
+    })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
