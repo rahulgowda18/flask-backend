ca1ef0134e0c66d8e5c0f6e9265deb5d1a251009
Rahul gowda <gowdarahul185@gmail.com>
2026-02-14 14:33:52 +0530

    9

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Flask Backend Running with Docker"})
+    return "Flask Backend Running!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
